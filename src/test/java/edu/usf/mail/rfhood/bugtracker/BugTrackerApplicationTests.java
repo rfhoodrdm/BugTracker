e7ab8febde518f7784d9dc52d92849d2444b4027
roberthood <roberthood@pop-os.localdomain>
2020-03-26 20:04:47 -0400

    Initial creation of project.

---
+++ src/test/java/edu/usf/mail/rfhood/bugtracker/BugTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.usf.mail.rfhood.bugtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
